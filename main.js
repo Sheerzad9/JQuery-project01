1d5b0790faba8fe734b4d06b43c83e7da692d2d6
Ali Sheerzad <ali.sheerzad9@gmail.com>
2021-11-26 17:54:53 +0200

    added styling

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+    // Below is the API which returns data of the current incidents of the country 
+    //     fetch("https://covid-19-data.p.rapidapi.com/report/country/name?name=Finland&date=2020-04-01", {
+    // 	"method": "GET",
+    // 	"headers": {
+    // 		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+    // 		"x-rapidapi-key": "8c3dc06293msh94e84c30ec66810p1d8bb8jsn7852d910f2a0"
+    // 	}
+    // })
+    // .then(response => {
+    // 	console.log(response);
+    //     return response.json()
+    // }).then((data) => {
+    //     console.log(data[0]);
+    // })
+    // .catch(err => {
+    // 	console.error(err);
+    // });
+
+    //////////////////////////////////////////////////////////////////////
+    // API which returns data of safety travel rate of searched country
+    // fetch("https://eu-covid-19-travel.p.rapidapi.com/travel/Finland", {
+	// "method": "GET",
+	// "headers": {
+	// 	"x-rapidapi-host": "eu-covid-19-travel.p.rapidapi.com",
+	// 	"x-rapidapi-key": "8c3dc06293msh94e84c30ec66810p1d8bb8jsn7852d910f2a0"
+	// }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    //     return response.json()
+    // }).then((data) => {
+    //     console.log(data[0]);
+    // })
+    // .catch(err => {
+    //     console.error(err);
+    // });
+});
